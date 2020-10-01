c90dfcdf590141be144acac76e58d3505280b329
julia <s98435@yandex.ru>
2020-10-01 18:10:38 +0300

    feat: write all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,77 @@
 
 exports.min = function min (array) {
+
+  if (array) {
+      if (array.length == 0 ) {
+        return 0;
+      }
+
+    let minValue = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] < minValue) {
+          minValue = array[i];
+        }
+    }
+
+      return minValue;
+  };
+
   return 0;
 }
 
 exports.max = function max (array) {
+
+  if (array) {
+
+      if (array.length == 0) {
+        return 0;
+      }
+
+    let maxValue = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] > maxValue) {
+          maxValue = array[i];
+        }
+    }
+
+      return maxValue;
+  };
+
   return 0;
 }
 
+
+
 exports.avg = function avg (array) {
-  return 0;
-}
+
+  if (array) {
+
+    if (array.length == 0) {
+      return 0;
+    }
+
+    let varNumber = 0;
+    let sumValue = 0;
+
+    for (let i = 0; i < array.length; i++) {
+
+      if (typeof array[i] == 'number') {
+        sumValue += array[i];
+        varNumber++;
+      }
+    }
+
+    let avrNumber = sumValue / varNumber;
+
+    return  avrNumber;
+  }
+
+return 0;
+
+};
+
+
